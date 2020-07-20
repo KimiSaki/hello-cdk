3b4a3b73a953bc1e4da0759f7f582b99c465a976
Saki Kimiduka <s.kimizuka@hotmail.co.jp>
2020-07-20 16:10:10 +0900

    first commit.

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import core
+from aws_cdk import aws_s3 as _s3
 
 
 class HelloCdkStack(core.Stack):
@@ -7,3 +8,4 @@ class HelloCdkStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        bucket = _s3.Bucket(self, "MyFirstBucket", bucket_name="kimi-first-cdk-bucket")
